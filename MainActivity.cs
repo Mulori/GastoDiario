3323e701223abfaa9d959a1532fb94745123a595
mulori <murilogarcia.hr@gmail.com>
2021-04-24 15:53:42 -0300

    FirtCommit

---
+++ MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace GastoDiario
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ImageView img;
+        TextView lbEmail;
+        TextView lbSenha;
+        EditText txtEmail;
+        EditText txtSenha;
+        TextView btnRegistrar;
+        Button btnEntrar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            img = FindViewById<ImageView>(Resource.Id.imageViewLogo);
+            txtEmail = FindViewById<EditText>(Resource.Id.edtEmail);
+            txtSenha = FindViewById<EditText>(Resource.Id.edtSenha);
+
+            lbEmail = FindViewById<TextView>(Resource.Id.txtEmail);
+            lbEmail.Text = "Email";
+
+            lbSenha = FindViewById<TextView>(Resource.Id.txtSenha);
+            lbSenha.Text = "Senha";
+
+            btnRegistrar = FindViewById<TextView>(Resource.Id.btnRegistrar);
+            btnRegistrar.Text = "Registra-se";
+            btnRegistrar.Click += btnRegistrar_Click;
+
+            btnEntrar = FindViewById<Button>(Resource.Id.btnEntrar);
+            btnEntrar.Text = "Entrar";
+            btnEntrar.Click += btnEntrar_Click;
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        void btnRegistrar_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        void btnEntrar_Click(object sender, System.EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                Toast.MakeText(Application.Context, "O campo email está em branco!", ToastLength.Long).Show();
+                return;
+            }
+
+            if (txtSenha.Text == "")
+            {
+                Toast.MakeText(Application.Context, "O campo senha está em branco!", ToastLength.Long).Show();
+                return;
+            }
+        }
+    }
+}
---
+++ obj/Debug/110/android/src/crc640449b8381a29da6c/RegisterActivity.java
@@ -0,0 +1,56 @@
+package crc640449b8381a29da6c;
+
+
+public class RegisterActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"";
+		mono.android.Runtime.register ("GastoDiario.RegisterActivity, GastoDiario", RegisterActivity.class, __md_methods);
+	}
+
+
+	public RegisterActivity ()
+	{
+		super ();
+		if (getClass () == RegisterActivity.class)
+			mono.android.TypeManager.Activate ("GastoDiario.RegisterActivity, GastoDiario", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
